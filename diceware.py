ff99574c093f7839c3c387abce072b7ee161fd85
Petri Lehtinen <petri@digip.org>
2008-11-30 00:35:45 +0200

    refactor to an importable module

--- diceware.py
+++ diceware.py
@@ -58,146 +58,169 @@ WORD_LIST_URLS = {
 }
 
 
-def ensure_dir(path):
-    """Ensure that path is a directory creating it if necessary.
+def generate(word_list, words=5, specials=0):
+    rnd = SystemRandom()
+    words = [ rnd.choice(word_list) for _ in range(words) ]
 
-    If path already exists and is not a directory, print an error
-    message and quit with sys.exit().
+    # Insert at most options.special special characters. This is not
+    # exactly the procedure described in the Diceware web page, because
+    # this handles the case where there are more than 6 words in the
+    # passphrase and more than 6 characters in the word.
+    if specials:
+        split_words = [ map(None, x) for x in words ]
+        for _ in range(specials):
+            # i is the index of the word in which the special character
+            # replacement takes place.
+            i = rnd.randrange(len(split_words))
 
-    Parameters:
-      path   String specifying the path to ensure
+            # j is the index of the character to be replaced with a special
+            # character.
+            j = rnd.randrange(len(split_words[i]))
 
-    Return value:
-      path
-    
-    """
-    if not os.path.exists(path):
-        os.makedirs(path)
-    elif not os.path.isdir(path):
-        print("error: '%s' is not a directory" % path)
-        sys.exit(1)
-    return path
+            # k is the index of the special character
+            k = rnd.randrange(len(SPECIAL_CHARS))
 
+            # Split to individual characters, replace the k'th char, unsplit
+            split_words[i][j] = SPECIAL_CHARS[k]
 
-config_dir = ensure_dir(os.path.expanduser("~/.diceware.py"))
-cache_dir = ensure_dir(os.path.join(config_dir, "cache"))
+        with_specials = [ "".join(x) for x in split_words ]
+    else:
+        with_specials = words
 
-# Parse config file
-config_file = os.path.join(config_dir, "config")
-config = SafeConfigParser()
-config.read(config_file)
-
-def config_default(config, section, option, default):
-    """Set default values for options that do not have a value."""
-    try:
-        config.get(section, option)
-    except NoSectionError:
-        config.add_section(section)
-        config.set(section, option, default)
-    except NoOptionError:
-        config.set(section, option, default)
-
-config_default(config, "defaults", "lang", "en")
-config_default(config, "defaults", "words", "5")
-config_default(config, "defaults", "special", "0")
-config_default(config, "defaults", "file", "")
-
-# Sanity checks for config options
-if config.get("defaults", "lang") not in WORD_LIST_URLS.keys():
-    print("error: '%s' is not a valid value for option 'lang'"
-          % config.get("defaults", "lang"))
-    sys.exit(1)
-try:
-    config.getint("defaults", "words")
-    config.getint("defaults", "special")
-except ValueError:
-    print("error: 'words' and 'special' options must have integer values")
-    sys.exit(1)
+    return words, with_specials
 
 
-# Parse command line arguments
-parser = OptionParser()
-parser.add_option("-n", "--words", dest="words", type="int", metavar="N",
-                  help="generate N words (default: %default)",
-                  default=config.getint("defaults", "words"))
-parser.add_option("-s", "--special", dest="special", type="int", metavar="M",
-                  help="insert M special characters (default: %default)",
-                  default=config.getint("defaults", "special"))
-parser.add_option("-f", "--file", dest="file", metavar="FILE",
-                  help="override the `lang' option and read the word list " +
-                  "from FILE", default=config.get("defaults", "file"))
-linguas = sorted(WORD_LIST_URLS.keys())
-parser.add_option("-l", "--lang", dest="lang", metavar="LANG",
-                  type="choice", choices=linguas,
-                  help="use the word list for LANG (" + ", ".join(linguas) +
-                  ") (default: %default)", default=config.get("defaults", "lang"))
+def read_word_list(fobj):
+    # Read the word list skipping lines which do not start with 5 digits
+    # and a white space character and removing the 5 digits
+    word_list = [ line[6:].strip() for line in fobj
+                 if line[0:5].isdigit() and line[5].isspace() ]
 
-options, args = parser.parse_args()
-if args or options.words < 1 or options.special < 0:
-    parser.print_help()
-    sys.exit(0)
+    # A valid Diceware word list has exactly 6**5 = 7776 words
+    if len(word_list) != 7776:
+        raise ValueError("invalid word list format")
 
-parser.destroy()
+    return word_list
 
-# --file has higher precedence than --lang
-if options.file:
-    try: fobj = open(options.file)
-    except IOError:
-        print("error: unable to open word list file '%s'" % options.file)
-        sys.exit(1)
-else:
+
+def get_word_list(cache_dir, lang="en"):
+    assert lang in WORD_LIST_URLS.keys()
+
     # Read the cached word list
-    word_list_path = os.path.join(cache_dir, options.lang)
+    word_list_path = os.path.join(cache_dir, lang)
     try:
         fobj = open(word_list_path)
     except IOError:
         # The word list does not exist => cache it
-        word_list_url = WORD_LIST_URLS[options.lang]
-        try: urllib.urlretrieve(word_list_url, word_list_path)
-        except IOError:
-            print("error: unable to open remote word list '%s'" % word_list_url)
-            sys.exit(1)
+        word_list_url = WORD_LIST_URLS[lang]
+        urllib.urlretrieve(word_list_url, word_list_path)
         fobj = open(word_list_path)
 
+    return read_word_list(fobj)
 
-# Read the word list skipping lines which do not start with 5 digits
-# and a white space character and removing the 5 digits
-word_list = [ line[6:].strip() for line in fobj
-             if line[0:5].isdigit() and line[5].isspace() ]
-fobj.close()
 
-# A valid Diceware word list has exactly 6**5 = 7776 words
-if len(word_list) != 7776:
-    print("error: invalid word list format")
-    sys.exit(1)
+def main():
+    def ensure_dir(path):
+        """Ensure that path is a directory creating it if necessary.
 
-# Initialize the random source
-rnd = SystemRandom()
+        If path already exists and is not a directory, print an error
+        message and quit with sys.exit().
 
-# Generate passphrase
-words = [ rnd.choice(word_list) for _ in range(options.words) ]
-print("passphrase   : %s" % " ".join(words))
+        Parameters:
+          path   String specifying the path to ensure
 
-# Insert at most options.special special characters. This is not
-# exactly the procedure described in the Diceware web page, because
-# this handles the case where there are more than 6 words in the
-# passphrase and more than 6 characters in the word.
-for _ in range(options.special):
-    # i is the index of the word in which the special character
-    # replacement takes place.
-    i = rnd.randrange(options.words)
+        Return value:
+          path
 
-    # j is the index of the character to be replaced with a special
-    # character.
-    j = rnd.randrange(len(words[i]))
+        """
+        if not os.path.exists(path):
+            os.makedirs(path)
+        elif not os.path.isdir(path):
+            print("error: '%s' is not a directory" % path)
+            sys.exit(1)
+        return path
 
-    # k is the index of the special character
-    k = rnd.randrange(36)
 
-    # Split to individual characters, replace the k'th char, unsplit
-    word = map(None, words[i])
-    word[j] = SPECIAL_CHARS[k]
-    words[i] = "".join(word)
+    def config_default(config, section, option, default):
+        """Set default values for options that do not have a value."""
+        try:
+            config.get(section, option)
+        except NoSectionError:
+            config.add_section(section)
+            config.set(section, option, default)
+        except NoOptionError:
+            config.set(section, option, default)
 
-if options.special > 0:
-    print("with specials: %s" % " ".join(words))
+
+    config_dir = ensure_dir(os.path.expanduser("~/.diceware.py"))
+    cache_dir = ensure_dir(os.path.join(config_dir, "cache"))
+
+    # Parse config file
+    config_file = os.path.join(config_dir, "config")
+    config = SafeConfigParser()
+    config.read(config_file)
+
+    config_default(config, "defaults", "lang", "en")
+    config_default(config, "defaults", "words", "5")
+    config_default(config, "defaults", "special", "0")
+    config_default(config, "defaults", "file", "")
+
+    # Sanity checks for config options
+    if config.get("defaults", "lang") not in WORD_LIST_URLS.keys():
+        print("error: '%s' is not a valid value for option 'lang'"
+              % config.get("defaults", "lang"))
+        sys.exit(1)
+    try:
+        config.getint("defaults", "words")
+        config.getint("defaults", "special")
+    except ValueError:
+        print("error: 'words' and 'special' options must have integer values")
+        sys.exit(1)
+
+    # Parse command line arguments
+    parser = OptionParser()
+    parser.add_option("-n", "--words", dest="words", type="int", metavar="N",
+                      help="generate N words (default: %default)",
+                      default=config.getint("defaults", "words"))
+    parser.add_option("-s", "--special", dest="special", type="int", metavar="M",
+                      help="insert M special characters (default: %default)",
+                      default=config.getint("defaults", "special"))
+    parser.add_option("-f", "--file", dest="file", metavar="FILE",
+                      help="override the `lang' option and read the word list " +
+                      "from FILE", default=config.get("defaults", "file"))
+    linguas = sorted(WORD_LIST_URLS.keys())
+    parser.add_option("-l", "--lang", dest="lang", metavar="LANG",
+                      type="choice", choices=linguas,
+                      help="use the word list for LANG (" + ", ".join(linguas) +
+                      ") (default: %default)", default=config.get("defaults", "lang"))
+
+    options, args = parser.parse_args()
+    if args or options.words < 1 or options.special < 0:
+        parser.print_help()
+        sys.exit(0)
+
+    parser.destroy()
+
+    # --file has higher precedence than --lang
+    if options.file:
+        try:
+            fobj = open(options.file)
+        except IOError:
+            print("error: unable to open word list file '%s'" % options.file)
+            sys.exit(1)
+        try:
+            word_list = read_word_list(fobj)
+        except ValueError, e:
+            print("error: %s" % e)
+            sys.exit(1)
+    else:
+        word_list = get_word_list(cache_dir, options.lang)
+
+    words, with_specials = generate(word_list, options.words, options.special)
+    print("passphrase   : %s" % " ".join(words))
+    if options.special > 0:
+        print("with specials: %s" % " ".join(with_specials))
+
+
+if __name__ == "__main__":
+    main()
